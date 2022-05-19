5ed48f089fcb59900523c61a79c80f2a419ddcbc
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-05-19 16:27:36 +0200

    Initial commit

---
+++ Hackaton/src/test/java/cat/itacademy/barcelonactiva/hackaton/HackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ teste.sql
@@ -0,0 +1,9 @@
+create table terrasses (
+id INT primary key,
+cod_barrio INT,
+name_barrio varchar(40),
+dirrecion varchar(120),
+superficie decimal(4, 2),
+mesas int,
+sillas int,
+latitud )
